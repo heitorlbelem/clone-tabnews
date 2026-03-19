de6b88117be2452adf3d7dd04cd02f676bf17192
Heitor Belem <32557513+heitorlbelem@users.noreply.github.com>
2026-03-19 20:03:34 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,26 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  return response.status(200).json({ message: "Hello World!" });
+  const databaseVersion = await database.query("SHOW SERVER_VERSION;");
+  const databaseMaxConnections = await database.query("SHOW max_connections");
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenConnections = await database.query({
+    text: "SELECT count(*)::int as opened_connections from pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+  });
+
+  return response.status(200).json({
+    updated_at: new Date().toISOString(),
+    dependencies: {
+      database: {
+        version: databaseVersion.rows[0].server_version,
+        max_connections: new Number(
+          databaseMaxConnections.rows[0].max_connections,
+        ),
+        opened_connections: databaseOpenConnections.rows[0].opened_connections,
+      },
+    },
+  });
 }
 
 export default status;
