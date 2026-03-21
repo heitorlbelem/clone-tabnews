1d2c7c6540c94afc9fb1b5b68870face07befbe3
Heitor Belém <heitorlbelem98@gmail.com>
2026-03-21 02:10:13 -0300

    creates migrations endpoint

---
+++ tests/integrations/api/v1/migrations/post.test.js
@@ -0,0 +1,16 @@
+import database from "infra/database";
+
+const cleanDatabase = async () => {
+  await database.query("drop schema public cascade; create schema public;");
+};
+
+beforeAll(cleanDatabase);
+
+test("POST to /api/v1/migrations should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "POST",
+  });
+  expect(response.status).toBe(200);
+  const responseBody = await response.json();
+  expect(responseBody.length).toBeGreaterThan(0);
+});
