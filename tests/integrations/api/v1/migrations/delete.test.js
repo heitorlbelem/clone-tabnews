ffb5ce0f5d688ad47520b20a23218c4fa270662b
Heitor Belém <heitorlbelem98@gmail.com>
2026-04-18 02:16:08 -0300

    test: rename all descriptions to use new pattern

--- tests/integrations/api/v1/migrations/delete.test.js
+++ tests/integrations/api/v1/migrations/delete.test.js
@@ -6,11 +6,15 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("DELETE to /api/v1/migrations should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "DELETE",
+describe("DELETE /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Deleting migrations", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "DELETE",
+      });
+      expect(response.status).toBe(405);
+      const responseBody = await response.json();
+      expect(responseBody.error).toEqual('Method "DELETE" not allowed');
+    });
   });
-  expect(response.status).toBe(405);
-  const responseBody = await response.json();
-  expect(responseBody.error).toEqual('Method "DELETE" not allowed');
 });
